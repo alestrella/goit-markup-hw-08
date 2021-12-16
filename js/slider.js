c406b54908d999f0e99854940b631c6be2bbc257
Alexandra Sabatin <s4batin@gmail.com>
2021-12-17 01:00:02 +0200

    Fix slider

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,7 @@ var swiper = new Swiper(".mySwiper", {
           delay: 2500,
           disableOnInteraction: false,
         },
+        loop: true,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
