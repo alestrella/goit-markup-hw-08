0b07b6df45030b2eb363d644ed86d2d6acdfff89
Alexandra Sabatin <s4batin@gmail.com>
2021-12-16 23:17:13 +0200

    Add a slider to Clients clock

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+  spaceBetween: 10,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 6,
+            spaceBetween: 30,
+          },
+        },
+      });
